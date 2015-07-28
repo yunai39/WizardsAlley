67105b02e3ea6df1d54a4ee40258ea458528a2be
Yunai39 <yunai39@gmail.com>
2015-07-28 20:50:51 +0200

    Ajout des scripts pour la creation des utilisateurs, un utilisateur admin pwd admin

---
+++ script-sql/WizardUser.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `wizard_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
+(2, 'admin', 'admin', 'admin@admin.com', 'admin@admin.com', 1, 'tlfxbhjrrlwkog8s40c4o8occgg0kwg', 'jlxj+OHfQHuTv5KZwRYLnmuak1tKdKFlLp4t+U9MU9Do4LkgBGujsOGTd956/mlw8+5e/StwyDvAyUYIDOlHOg==', '2015-07-27 22:11:08', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL);
