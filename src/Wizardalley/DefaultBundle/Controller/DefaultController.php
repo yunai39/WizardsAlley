90ed0dadb1c67861fadd296a184255def30181dd
Yunai39 <yunai39@gmail.com>
2015-07-29 07:40:26 +0200

    Ajoute des pages copiright, mention etc

--- src/Wizardalley/DefaultBundle/Controller/DefaultController.php
+++ src/Wizardalley/DefaultBundle/Controller/DefaultController.php
@@ -8,6 +8,18 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('WizardalleyDefaultBundle:Default:index.html.twig', array('name' => 'Harry'));
+        return $this->render('WizardalleyDefaultBundle:Default:index.html.twig');
+    }
+    public function mentionAction()
+    {
+        return $this->render('WizardalleyDefaultBundle:Default:mention.html.twig');
+    }
+    public function copyrightAction()
+    {
+        return $this->render('WizardalleyDefaultBundle:Default:copyright.html.twig');
+    }
+    public function confidentialityAction()
+    {
+        return $this->render('WizardalleyDefaultBundle:Default:confidentiality.html.twig');
     }
 }
