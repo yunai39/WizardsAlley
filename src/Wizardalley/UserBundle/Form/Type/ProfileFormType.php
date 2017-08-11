9756b945ad344c087db6a30421dd121194d30010
Yunai39 <yunai39@gmail.com>
2017-08-11 20:18:57 +0200

    Modification du profile

--- src/Wizardalley/UserBundle/Form/Type/ProfileFormType.php
+++ src/Wizardalley/UserBundle/Form/Type/ProfileFormType.php
@@ -1,61 +1,71 @@
 <?php
 
-/*
- * This file is part of the FOSUserBundle package.
- *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
+namespace Wizardalley\UserBundle\Form\Type;
 
-namespace Wizardalley\UserBundle\Form\Type; 
-
-use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseForm;
 
 /**
  * Class ProfileFormType
+ *
  * @package Wizardalley\UserBundle\Form\Type
  */
-class ProfileFormType extends Form
+class ProfileFormType extends BaseForm
 {
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('facebook','text',array(
-                'label' =>'wizard.register.label.facebook',
-                )
+            ->remove('username')
+            ->remove('email')
+            ->remove('current_password')
+            ->add(
+                'lastname',
+                'text',
+                [
+                    'label' => 'wizard.register.label.lastname',
+                ]
             )
-            ->add('twitter','text',array(
-                'label' =>'wizard.register.label.twitter',
-                )
+            ->add(
+                'firstname',
+                'text',
+                [
+                    'label' => 'wizard.register.label.firstname',
+                ]
             )
-
+            ->add(
+                'facebook',
+                'text',
+                [
+                    'label' => 'wizard.register.label.facebook',
+                    'required' => false
+                ]
+            )
+            ->add(
+                'twitter',
+                'text',
+                [
+                    'label' => 'wizard.register.label.twitter',
+                    'required' => false
+                ]
+            )
         ;
-
     }
 
-    /**
-     * @param OptionsResolverInterface $resolver
-     */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    public function getParent()
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'WizardalleyCoreBundle:WizardUser',
-        ));
+        return 'FOS\UserBundle\Form\Type\ProfileFormType';
     }
 
     /**
      * @return string
      */
-    public function getName()     
-    {         
-        return 'wizard_user_profile_edit';     
+    public function getName()
+    {
+        return 'wizard_user_profile_edit';
     }
 }
