edbf5dff04cf90b413c0ea069cfbd64efc07349d
Yunai39 <yunai39@gmail.com>
2017-11-01 18:45:40 +0100

    Issue 78

--- src/Wizardalley/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Wizardalley/UserBundle/Form/Type/RegistrationFormType.php
@@ -1,34 +1,55 @@
 <?php
-namespace Wizardalley\UserBundle\Form\Type;  
+namespace Wizardalley\UserBundle\Form\Type;
 
-use Symfony\Component\Form\FormBuilderInterface; 
+use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
-class RegistrationFormType extends BaseType {     
+class RegistrationFormType extends BaseType
+{
 
-    public function buildForm(FormBuilderInterface $builder, array $options)     
-    {         
-        parent::buildForm($builder, $options);         
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm(
+            $builder,
+            $options
+        );
 
         // add your custom field        
-        $builder->add('lastname','text',array(
-                'label' =>'wizard.register.label.lastname',
-            ))
-            ->add('firstname','text',array(
-                'label' =>'wizard.register.label.firstname',
-            ))
-            ->add('accept_cgv', 'checkbox')
-            ->add('sexe','choice', array(
-                'label'     => 'wizard.register.label.sexe',
-                'choices'   => array(
-                    '0'   => 'wizard.register.field.sexe.male',
-                    '1'   => 'wizard.register.field.sexe.female',
-                ))
-            );
+        $builder->add(
+            'lastname',
+            'text',
+            [
+                'label' => 'wizard.register.label.lastname',
+            ]
+        )
+                ->add(
+                    'firstname',
+                    'text',
+                    [
+                        'label' => 'wizard.register.label.firstname',
+                    ]
+                )
+                ->add(
+                    'accept_cgv',
+                    'checkbox',
+                    ['label' => 'wizard.register.field.cgv.accept']
+                )
+                ->add(
+                    'sexe',
+                    'choice',
+                    [
+                        'label' => 'wizard.register.label.sexe',
+                        'choices' => [
+                            '0' => 'wizard.register.field.sexe.male',
+                            '1' => 'wizard.register.field.sexe.female',
+                        ]
+                    ]
+                )
+        ;
     }
 
-    public function getName()     
-    {         
+    public function getName()
+    {
         return 'wizard_user_registation';
-    } 
+    }
 }
