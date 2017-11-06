6f51e88d392a5c5cec2be246c64f3342d95b7295
Yunai39 <yunai39@gmail.com>
2017-11-06 07:59:00 +0100

    Ajout d'un token pour l'alpha

--- src/Wizardalley/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Wizardalley/UserBundle/Form/Type/RegistrationFormType.php
@@ -3,9 +3,48 @@ namespace Wizardalley\UserBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
+/**
+ * Class RegistrationFormType
+ *
+ * @package Wizardalley\UserBundle\Form\Type
+ */
 class RegistrationFormType extends BaseType
 {
+    /**
+     * @var
+     */
+    protected $tokenAlpha;
+
+    /**
+     * RegistrationFormType constructor.
+     *
+     * @param string $class \
+     */
+    public function __construct($class, $tokenAlpha)
+    {
+        parent::__construct($class);
+        $this->tokenAlpha = $tokenAlpha;
+    }
+
+    /**
+     * @param                           $value
+     * @param ExecutionContextInterface $context
+     */
+    public function validateAlpha($value, ExecutionContextInterface $context)
+    {
+        $form = $context->getRoot();
+
+        if ($value != $this->tokenAlpha) {
+            $context
+                ->buildViolation('La code pour accéder a la version alpha n\'est pas valide')
+                ->addViolation()
+            ;
+        }
+    }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -38,16 +77,32 @@ class RegistrationFormType extends BaseType
                     'sexe',
                     'choice',
                     [
-                        'label' => 'wizard.register.label.sexe',
+                        'label'   => 'wizard.register.label.sexe',
                         'choices' => [
                             '0' => 'wizard.register.field.sexe.male',
                             '1' => 'wizard.register.field.sexe.female',
                         ]
                     ]
                 )
+                ->add(
+                    'alpha_code',
+                    'text',
+                    [
+                        'label'       => 'wizard.register.label.token_alpha',
+                        'mapped'      => false,
+                        'required'    => true,
+                        'constraints' => [
+                            new NotBlank(),
+                            new Callback([$this, 'validateAlpha']),
+                        ],
+                    ]
+                )
         ;
     }
 
+    /**
+     * @return string
+     */
     public function getName()
     {
         return 'wizard_user_registation';
